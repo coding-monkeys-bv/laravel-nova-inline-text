22658bf81cf8380e8244b79d8dac14d928d0a5a8
Michael Blijleven <michael@voicecode.nl>
2022-07-02 18:25:16 +0200

    Hello world

---
+++ resources/js/components/IndexField.vue
@@ -0,0 +1,74 @@
+<template>
+    <input
+        :id="field.name + field.id"
+        type="text"
+        class="w-full form-control form-input form-input-bordered inline-text-field"
+        :placeholder="field.name"
+        v-model="value"
+        @blur="save"
+        @keyup.enter="save"
+        :style="'min-width:'+field.minWidth+'px;'"
+    />
+</template>
+
+<script>
+import { FormField } from 'laravel-nova'
+
+export default {
+    props: ['resourceName', 'field'],
+
+    mixins: [FormField],
+
+    data() {
+        return {
+            loading: false,
+        }
+    },
+
+    methods: {
+        save() {
+            var vm = this;
+            if (this.value != this.field.value) {
+                if (!this.loading) {
+
+                    Nova.request().post(`/inline-text/update/${this.resourceName}`, {
+                        id: this.field.id,
+                        attribute: this.field.attribute,
+                        value: this.value
+                    }).then (function (response) {
+                        vm.loading = false
+                        vm.field.value = vm.value
+
+                        Nova.success(vm.field.successMessage);
+                    }).catch (function (error) {
+                        vm.loading = false
+
+                        if (error.response.status == 422) {
+                            let validationErrors = [];
+
+                            error.response.data.errors.value.forEach(function (errorMessage) {
+                                validationErrors.push(errorMessage.replace('value', vm.field.name));
+                            });
+
+                            Nova.error(validationErrors.join('<br>'));
+                        }
+                    })
+                }
+                this.loading = true;
+            }
+        },
+
+        setInitialValue() {
+            this.value = String(this.field.value) || ''
+        },
+
+        fill(formData) {
+            formData.append(this.field.attribute, this.value || '')
+        },
+
+        handleChange(value) {
+            this.value = value
+        },
+    },
+}
+</script>
---
+++ src/InlineText.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CodingMonkeysBv\InlineText;
+
+use Laravel\Nova\Fields\Field;
+
+class InlineText extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'inline-text';
+
+    /**
+     * Resolve the given attribute from the given resource.
+     *
+     * @param  mixed  $resource
+     * @param  string  $attribute
+     * @return mixed
+     */
+    protected function resolveAttribute($resource, $attribute)
+    {
+        $this->setResourceId(data_get($resource, $resource->getKeyName()));
+
+        return parent::resolveAttribute($resource, $attribute);
+    }
+
+    /**
+     * Set success message.
+     *
+     * @param Closure|string $message
+     * @return $this
+     */
+    public function successMessage($message = 'Successfully updated!')
+    {
+        return $this->withMeta(['successMessage' => $message]);
+    }
+
+    /**
+     * Set resource ID.
+     *
+     * @param int $id
+     * @return void
+     */
+    protected function setResourceId($id)
+    {
+        return $this->withMeta([
+            'id' => $id,
+            'nova_path' => config('nova.path'),
+        ]);
+    }
+
+    /**
+     * Set minimum with of input.
+     *
+     * @param int $width
+     * @return void
+     */
+    public function minWidth($width = 100)
+    {
+        return $this->withMeta([
+            'minWidth' => $width,
+        ]);
+    }
+}
